09dd81573edbe84c583d1754258d757f1eb4f357
Ahamefula Nzubechi <nzube1999@gmail.com>
2022-07-06 23:00:57 +0000

    check challenge 1,challenge 2 and challenge 3

--- src/assignment.js
+++ src/assignment.js
@@ -13,11 +13,22 @@ const assignment = {};
  * @param {Array} arrayOfNumbers the array of numbers to sum
  * @returns number the sum of the numbers
  */
-function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
-}
-// assignment.sumOfNumbers = sumOfNumbers;
+let arrayOfNumbers = [10, 12, 31, 43, 44, 65];
 
+
+function sumOfNumbers(arrayOfNumbers){
+    let sum = 0;
+
+
+for (let i = 0; i < arrayOfNumbers.length; i++) 
+   sum += arrayOfNumbers[i]
+    return sum;
+
+
+}
+// console.log(sumOfNumbers(arrayOfNumbers));
+
+assignment.sumOfNumbers = sumOfNumbers;
 /**
  * Challenge - 2
  * 
@@ -27,10 +38,27 @@ function sumOfNumbers(arrayOfNumbers) {
  * @param {Array} arrayOfNumbers the array containing even or non-even numbers
  * @returns number the count of even numbers
  */
+// function countEvenNumbers(arrayOfNumbers) {
+//     return arrayOfNumbers.length;
+// }
+// let arrayOfNumbers = [10, 12, 31, 43, 44, 65];
+
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let evenCount = 0
+
+    for(let i = 0; i < arrayOfNumbers.length; i++){
+        if(arrayOfNumbers[i] % 2 === 0){
+             evenCount++;
+
+        }
+        
+        }
+        return evenCount
+       
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+// console.log(countEvenNumbers(arrayOfNumbers))
+
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
@@ -46,10 +74,19 @@ function countEvenNumbers(arrayOfNumbers) {
  * @param {Array} arrayOfNumbers the array containing temperatures in Celsius to be converted
  * @returns Array the converted temperatures in Fahrenheit
  */
-function celsiusToFahrenheit(arrayOfNumbers) {
-    return arrayOfNumbers;
+// function celsiusToFahrenheit(arrayOfNumbers) {
+//     return arrayOfNumbers;
+// }
+// let arrayOfNumbers = [10, 12, 31, 43, 44, 65];
+
+function celsiusToFahrenheit(arrayOfNumbers){
+    let celsius = arrayOfNumbers.map(array => Math.trunc( array * 9/5 + 32)) 
+    return celsius
+  
 }
-// assignment.celsiusToFahrenheit = celsiusToFahrenheit;
+// console.log(celsiusToFahrenheit(arrayOfNumbers));  
+
+assignment.celsiusToFahrenheit = celsiusToFahrenheit;
 
 
 // ========================
